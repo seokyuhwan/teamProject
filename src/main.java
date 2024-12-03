7451ec47d798f156a20ce29127bde58b57c5d688
kimzieun <10wldms14@gmail.com>
2024-12-03 14:14:11 +0900

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+
+public class main extends JFrame {
+    private JButton stopWatch, search, next, prev;
+    private JCheckBox filter;
+    public main(){
+
+    }
+    class recipe extends JPanel{
+        public recipe(){
+
+        }
+    }
+    class imageViewer extends JPanel{
+        public imageViewer(){
+
+        }
+    }
+    class filter extends JPanel{
+        public filter(){
+
+        }
+    }
+
+}
